63d3c18d085a521b43e66fbfbe8180e3e3096b77
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-03-13 07:19:05 +0300

    Add files via upload

---
+++ localization.lua
@@ -0,0 +1,48 @@
+Wli.vars["localTbl"] = {}
+
+if (GetLocale() == "ruRU") then
+	Wli.vars["localTbl"]["loaded."] = "загружен.";
+	Wli.vars["localTbl"]["Type /whisplist show to display the whisps' list."] = "Введите /whisplist show, чтобы отобразить список.";
+	Wli.vars["localTbl"]["There is no saved list."] = "Нет сохраненного списка.";
+	Wli.vars["localTbl"]["Unable to find this player."] = "Не удается найти этого игрока.";
+	Wli.vars["localTbl"]["hidden. Type /whisplist show to show the list again."] = "скрыт. Введите /whisplist show, чтобы снова показать список.";
+	Wli.vars["localTbl"]["Incompatible saved list."] = "Несовместимый сохраненный список.";
+
+	--------- title dropdown menu
+	Wli.vars["localTbl"]["Sort by time"] = "Сортировать по времени";
+	Wli.vars["localTbl"]["Sort alphabetical"] = "Сортировать по алфавиту";
+	Wli.vars["localTbl"]["Mark all as replied"] = "Отметить все как отвеченные";
+	Wli.vars["localTbl"]["Empty list"] = "Очистить список";
+	Wli.vars["localTbl"]["Turn off sound"] = "Отключить звук";
+	Wli.vars["localTbl"]["Turn on sound"] = "Включить звук";
+
+	Wli.vars["localTbl"]["Load previous list"] = "Загрузить предыдущий список";
+	Wli.vars["localTbl"]["Collapse"] = "Свернуть";
+	Wli.vars["localTbl"]["Expand"] = "Развернуть";
+	Wli.vars["localTbl"]["Hide"] = "Скрыть";
+	Wli.vars["localTbl"]["Background"] = "Фон";
+	Wli.vars["localTbl"]["Cancel"] = "Отмена";
+	Wli.vars["localTbl"]["Reset"] = "Сбросить";
+
+	--------- name dropdown menu
+	Wli.vars["localTbl"]["Whisper"] = "Шепот";
+	Wli.vars["localTbl"]["Who"] = "Кто";
+	Wli.vars["localTbl"]["Mark as received / replied"] = "Отметить как получено / отвечено";
+	Wli.vars["localTbl"]["Delete"] = "Удалить";
+
+	--------- config UI
+	Wli.vars["localTbl"]["Show list"] = "Показать список";
+	Wli.vars["localTbl"]["Hide list"] = "Скрыть список";
+
+	--------- online help
+	Wli.vars["localTbl"]["help"] = "помощь";
+	Wli.vars["localTbl"]["show/hide the whisps list"] = "показать/скрыть список";
+	Wli.vars["localTbl"]["on | off: turn received whisper sound on or off"] = "on | off: включить или выключить звук при получении шепота";
+	Wli.vars["localTbl"]["empty list"] = "очистить список";
+	Wli.vars["localTbl"]["reset addon interface"] = "сбросить интерфейс аддона";
+	Wli.vars["localTbl"]["reset all settings to default"] = "сбросить все настройки аддона по умолчанию";
+	Wli.vars["localTbl"]["display available slash commands"] = "отобразить доступные команды";
+
+	--------- tooltip
+	Wli.vars["localTbl"]["Shift-drag to move the frame.\nDouble-click to collapse/expand."] = "Перетаскивайте с зажатой Shift, чтобы переместить рамку.\nДвойной щелчок, чтобы свернуть/развернуть.";
+end
